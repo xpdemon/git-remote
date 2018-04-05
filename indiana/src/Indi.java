f4bb18c336a4815c2446476cbf0b6dee46a063ae
Bruno Boujenah <boujenah.bruno@gmail.com>
2018-04-05 18:10:36 +0200

    Indiana

---
+++ indiana/src/Indi.java
@@ -0,0 +1,16 @@
+public class Indi
+{
+
+    public static void main(String... args)
+    {
+        String title = "Indiana Jones and the Last Crusade";
+        boolean see = true;
+        int dateDeSorti = 1989;
+        double note = 8.3;
+
+        System.out.println(title);
+        System.out.println(see);
+        System.out.println(dateDeSorti);
+        System.out.println(note);
+    }
+}
